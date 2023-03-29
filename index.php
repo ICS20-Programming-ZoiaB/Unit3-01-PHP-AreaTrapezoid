6519c83e7587f3ded08c6d6080b85da709924b12
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-29 04:18:31 +0000

    added more code

---
+++ index.php
@@ -0,0 +1,49 @@
+<DOCTYPE html>
+<html lang="en-ca">
+  <head>
+    
+     <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="Area of Trapezoid Webpage">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="ZoiaB">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Code for the favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/android-chrome-512x512.png">
+    <link rel="manifest" href="./fav_index/android-chrome-512x512.png">
+    
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.pink-red.min.css" /> 
+    
+    <!-- Title -->
+    <title>Calculating the Area of a Trapezoid in PHP</title>
+  </head>
+  <body>
+    <?php echo "<h1>Calculating the Area of a Trapezoid in PHP with User Input</h1>"; ?>
+    
+    <!-- Div -->
+    <div id="div">
+      <?php
+        echo "<h3>Your Input</h3>";
+      ?>
+
+    <!-- Form for user input -->
+      <form action="./results.php" method="post" target="display-result">
+        <label for="height">Height of the trapezoid (in cm)</label>
+        <input type="float" name="height" placeholder="Height..."><br><br>
+        <label for="side-a">Length of side A (in cm)</label>
+        <input type="float" name="side-a" placeholder="Side A length..."><br><br>
+        <label for="side-b">Length of side B (in cm)</label>
+        <input type="float" name="side-b" placeholder="Side B length..."><br><br>
+        <input type="submit" value="Calculate" button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+      </form>
+    </div>
+    
+    <!-- Iframe to display results -->
+    <iframe id="display-result" name="display-result"></iframe>
+  </body>
+</html>
